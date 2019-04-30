b1876cd8a48b8549bdbedcbe32f6fca31de039a5
Arik Jones <tnypxl00@icloud.com>
2019-04-30 14:21:32 -0500

    Remove the beta flag

--- bin/install.sh
+++ bin/install.sh
@@ -11,7 +11,7 @@ printf "\n${grn}+ Installing Craft (This may take a few minutes)...${end}\n"
 # generate a new Craft app in the /tmp directory
 # and copy the generated files into the /app directory
 cd /tmp
-composer create-project craftcms/craft craft -s beta
+composer create-project craftcms/craft craft
 shopt -s dotglob
 cp -a craft/* /app
 cd -
